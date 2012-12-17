c8ae7fe99af4b88f860e76fb73272f31f5f01565
Kyle Lemons <kyle@kylelemons.net>
2012-12-16 23:31:50 -0500

    Add benchmarks

---
+++ bench_test.go
@@ -0,0 +1,36 @@
+package fatchan
+
+import (
+	"net"
+	"testing"
+)
+
+func discard(sid, cid uint64, err error) {}
+
+func BenchmarkE2E(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		a, b := net.Pipe()
+		axport, bxport := New(a, discard), New(b, discard)
+		ach, bch := make(chan string), make(chan string)
+		axport.FromChan(ach)
+		bxport.ToChan(bch)
+		ach <- "test"
+		<-bch
+		axport.Close()
+		bxport.Close()
+	}
+}
+
+func BenchmarkSend(bench *testing.B) {
+	a, b := net.Pipe()
+	axport, bxport := New(a, discard), New(b, discard)
+	ach, bch := make(chan string), make(chan string)
+	axport.FromChan(ach)
+	bxport.ToChan(bch)
+	for i := 0; i < bench.N; i++ {
+		ach <- "test"
+		<-bch
+	}
+	axport.Close()
+	bxport.Close()
+}
